5a9f5015be92b90b6bc33eb5188107c4e344dca3
Hardik <hardik.kotangale@gmail.com>
2023-03-18 15:13:38 +0530

    #Please enter the commit message for your changes. Lines starting   #with ' ' will be ignored, and an empty message aborts the commit.
      #On branch master
    
     # Changes to be committed:
     	new file:   .gitignore
     	new file:   README.md
     	new file:   package-lock.json
     	new file:   package.json
     	new file:   public/favicon.ico
     	new file:   public/index.html
     	new file:   public/logo192.png
     	new file:   public/logo512.png
     	new file:   public/manifest.json
     	new file:   public/robots.txt
     	new file:   server.js
     	new file:   src/App.css
     	new file:   src/App.js
     	new file:   src/App.test.js
     	new file:   src/assets/font/CentraNo2-Bold.ttf
     	new file:   src/assets/font/CentraNo2-Book.ttf
     	new file:   src/assets/font/CentraNo2-Medium.ttf
     	new file:   src/assets/img/Coditas.png
     	new file:   src/assets/img/MaxSecure.png
     	new file:   src/assets/img/Oytie-Blue.png
     	new file:   src/assets/img/Oytie.png
     	new file:   src/assets/img/RajaSoftwareLabs.png
     	new file:   src/assets/img/arrow1.svg
     	new file:   src/assets/img/arrow2.svg
     	new file:   src/assets/img/banner-bg-2.png
     	new file:   src/assets/img/banner-bg.png
     	new file:   src/assets/img/color-sharp2.png
     	new file:   src/assets/img/color-sharp5.png
     	new file:   src/assets/img/color-sharp7.png
     	new file:   src/assets/img/contact-img.png
     	new file:   src/assets/img/contact-img.svg
     	new file:   src/assets/img/footer-bg.png
     	new file:   src/assets/img/header-img.png
     	new file:   src/assets/img/nav-icon1.svg
     	new file:   src/assets/img/nav-icon2.svg
     	new file:   src/assets/img/nav-icon3.svg
     	new file:   src/assets/img/nav-icon4.png
     	new file:   src/assets/img/nav-icon5.png
     	new file:   src/assets/img/nav-icon6.png
     	new file:   src/assets/img/quickenSol.png
     	new file:   src/assets/img/zencon.png
     	new file:   src/assets/img/zensoft.png
     	new file:   src/components/Banner.js
     	new file:   src/components/Companies.js
     	new file:   src/components/Contact.js
     	new file:   src/components/Footer.js
     	new file:   src/components/Maps.js
     	new file:   src/components/NavBar.js
     	new file:   src/index.css
     	new file:   src/index.js
     	new file:   src/logo.svg
     	new file:   src/reportWebVitals.js
     	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Companies } from "./components/Companies";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Companies />
+      {/* <Projects /> */}
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
