c30a8620b714655570035bffd0076118ec20e047
Hardik <hardik.kotangale@gmail.com>
2023-04-23 16:18:55 +0530

    new file:   database.rules.json 	new file:   firestore.indexes.json 	new file:   firestore.rules 	new file:   functions/.eslintrc.js 	new file:   functions/.gitignore 	new file:   functions/index.js 	new file:   functions/package-lock.json 	new file:   functions/package.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/AboutUs.js 	new file:   src/components/Home.js 	new file:   src/components/JobPortal.js 	new file:   src/components/LogIn.js 	new file:   src/components/LoginModal.js 	new file:   src/components/Page404.js 	new file:   src/components/RegisterModal.js 	new file:   src/components/Services.js 	new file:   src/components/SignIn.js 	new file:   src/components/assets/img/Coditas.png 	new file:   src/components/assets/img/MaxSecure.png 	new file:   src/components/assets/img/Oytie-Blue.png 	new file:   src/components/assets/img/Oytie.png 	new file:   src/components/assets/img/RajaSoftwareLabs.png 	new file:   src/components/assets/img/arrow1.svg 	new file:   src/components/assets/img/arrow2.svg 	new file:   src/components/assets/img/banner-bg-2.png 	new file:   src/components/assets/img/banner-bg.png 	new file:   src/components/assets/img/color-sharp2.png 	new file:   src/components/assets/img/color-sharp5.png 	new file:   src/components/assets/img/color-sharp7.png 	new file:   src/components/assets/img/contact-img.png 	new file:   src/components/assets/img/contact-img.svg 	new file:   src/components/assets/img/footer-bg.png 	new file:   src/components/assets/img/header-img.png 	new file:   src/components/assets/img/nav-icon1.svg 	new file:   src/components/assets/img/nav-icon2.svg 	new file:   src/components/assets/img/nav-icon3.svg 	new file:   src/components/assets/img/nav-icon4.png 	new file:   src/components/assets/img/nav-icon5.png 	new file:   src/components/assets/img/nav-icon6.png 	new file:   src/components/assets/img/quickenSol.png 	new file:   src/components/assets/img/zencon.png 	new file:   src/components/assets/img/zensoft.png 	new file:   src/components/firebase.js 	new file:   src/components/homePage/Banner.js 	new file:   src/components/homePage/Companies.js 	new file:   src/components/homePage/Contact.js 	new file:   src/components/homePage/Footer.js 	new file:   src/components/homePage/Maps.js 	new file:   src/components/homePage/NavBar.js 	new file:   src/components/signInPage/Admin.js 	new file:   src/components/signInPage/Banner.js 	new file:   src/components/signInPage/NavBar.js 	new file:   src/components/signInPage/Student.js 	new file:   y/404.html 	new file:   y/index.html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Companies } from "./components/Companies";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import { NavBar } from "./components/homePage/NavBar";
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Page404 from './components/Page404';
+import Admin from './components/signInPage/Admin';
+import Student from './components/signInPage/Student';
+import AboutUs from './components/AboutUs';
+import JobPortal from './components/JobPortal';
+import Services from './components/Services';
+//import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Banner />
-      <Companies />
-      {/* <Projects /> */}
-      <Contact />
-      <Footer />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/logIn" element={<LogIn />} />
+        <Route path="/jobportal" element={<JobPortal/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/student" element={<Student />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
